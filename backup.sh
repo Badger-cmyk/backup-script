1c6b079aa9c4c2be6e610ae636e84b64bb9905fb
Eben <ebenbadger16@gmail.com>
2024-10-10 10:23:26 +0000

    updated backup.sh wrote code for backing up

---
+++ backup.sh
@@ -0,0 +1,19 @@
+if [ $# -ne 2 ]
+then
+    echo "Usage: ./backup.sh <source_directory> <target_directory>"
+    echo "Kindly try again."
+    exit 1
+fi
+
+if ! command -v rsync &> /dev/null
+then
+    echo "This script needs rsync to run."
+    echo "Kindly use your distribution's package manager to install the rsync command."
+    exit 2
+fi
+
+current_date=$(date +%Y-%m-%d)
+
+rsync_options="-ravb --backup-dir=$2/$current_date --delete --dry-run"
+
+$(which rsync) $rsync_options $1 $2 >> backup_$current_date.log
